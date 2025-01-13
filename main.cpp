fe669e94792fb064cdf1b0c4ec6ca4f56df93014
jherron4 <jherron4@ut.utm.edu>
2025-01-12 20:55:33 -0600

    Created my Hello World! program!

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Joseph Herron
+ * @date 2025-01-12
+ * @brief Quick demo of emacs stuff
+ * 
+ * Linux development review for dealing with emacs
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World" <<endl;
+  
+  return 0;
+}
+
